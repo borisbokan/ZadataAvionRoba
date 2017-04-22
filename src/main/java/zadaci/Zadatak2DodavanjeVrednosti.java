11a096044109d8678c7f50f8ee643640e76671f0
Bokan Boris <borisbokan@gmail.com>
2017-04-22 20:35:34 +0200

    Kreirane klase za zadatke fixes #3

---
+++ src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
@@ -0,0 +1,15 @@
+package zadaci;
+
+/**
+ * Created by borcha on 22.04.17..
+ */
+public class Zadatak2DodavanjeVrednosti {
+
+    private static myDB konekcija;
+
+    public static void main(String[] args){
+
+        konekcija=new myDB();//kokecija sa bazom
+
+    }
+}
---
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,15 @@
+package zadaci;
+
+/**
+ * Created by borcha on 22.04.17..
+ */
+public class Zadatak4BrisanjeVrednosti {
+
+    private static myDB konekcija;
+
+    public static void main(String[] args){
+
+        konekcija=new myDB();//kokecija sa bazom
+
+    }
+}
